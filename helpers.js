85cbb8b81ef6e870e4673a94fcbaba8285df29bd
Christopher Engelbart <cengelbart39@protonmail.com>
2025-04-07 11:16:55 -0400

    Initial Setup

---
+++ helpers.js
@@ -0,0 +1,1 @@
+// Place helper functions here
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home.jsx";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
