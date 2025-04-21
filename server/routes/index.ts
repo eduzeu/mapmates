30627a8ab268ebf02cf915856ffae18372581261
Eduardo Hernandez <112764648+eduzeu@users.noreply.github.com>
2025-04-21 11:49:00 -0400

    added route to index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,7 @@
 import restaurants from "./restaurants.ts";
 import reviews from "./reviews.ts";
 import express from "express";
-
+import users from "./users.ts";
 const constructorMethod = (app: express.Express) => {
   console.log("Registering /restaurants route");
   app.use("/restaurants", restaurants);
@@ -9,10 +9,14 @@ const constructorMethod = (app: express.Express) => {
   console.log("Registering /reviews route");
   app.use("/reviews", reviews);
 
+  console.log("Registering users route");
+  app.use("/users", users);
+
   console.log("Registering catch-all route");
   app.use((_: express.Request, res: express.Response) => {
     res.status(404).json({ error: "Not found" });
   });
+
 
   console.log("Registered all routes")
 }
